9fce1b3f6bbff15c5c3ed7b58c2d91346b44ce42
Timm Oleander Nielsen <timm0192@edu.easj.dk>
2020-04-24 17:36:08 +0200

    NOT FIXED

--- src/js/index.ts
+++ src/js/index.ts
@@ -4,24 +4,23 @@ import axios, {
 } from "../../node_modules/axios/index";
 
 interface Ibook {
-    id : number;
     title : string;
-    author : string;
-    publisher : string;
-    price : number;
+    artist : string;
+    releaseYear : number;
 }
 
 new Vue({
     el: "#app", 
     data: {
         books : [],
-        errors : []
+        errors : [],
+        search : ""
     },
 
     created()
     {
         console.log("CREATED")
-        axios.get<Ibook[]>("http://anbo-bookstorerest.azurewebsites.net/api/Books")
+        axios.get<Ibook[]>("https://restdrrecordstimm.azurewebsites.net/api/records")
             .then((response : AxiosResponse<Ibook[]>) => {
                 console.log(response.data)
                 this.books = response.data
@@ -33,24 +32,20 @@ new Vue({
 
     methods: {
 
+         bookSearch : function()
+        {
+            if(this.search != "" && this.search != null)
+            {
+                axios.get<Ibook[]>("https://restdrrecordstimm.azurewebsites.net/api/records?title="+ this.search) 
+                .then((response : AxiosResponse<Ibook[]>) =>{
+                    console.log(response.data)
+                    this.books = response.data
+                })
+                .catch((error : AxiosError) => {
+                    this.errors = error
+                })
+            }
+        } 
+
     }
 })
-
-
-
-function MakeLongHtmlList(data: Ibook[]):string
-{
-    let longHtml : string = "<table class='table'><thead class='thead-dark'><th>ID</th><th>TITEL</th><th>FORFATTER</th><th>FORLAG</th><th>PRIS</th>";
-    data.forEach(book => {
-        longHtml += "<tr>";
-        longHtml += "<td>" + book.id + "</td>"
-        longHtml += "<td>" + book.title + "</td>"
-        longHtml += "<td>" + book.author + "</td>"
-        longHtml += "<td>" + book.publisher + "</td>"
-        longHtml += "<td>" + book.price + "</td>"
-        longHtml += "</tr>"
-    });
-    longHtml += "</thead></table>";
-
-    return longHtml;
-}
