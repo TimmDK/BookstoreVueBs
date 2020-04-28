8f8b2f78e89191e4d06f3cbf692b7d7cea6b10b9
Timm Oleander Nielsen <timm0192@edu.easj.dk>
2020-04-28 14:52:39 +0200

    asodasdkadfm

--- src/js/index.ts
+++ src/js/index.ts
@@ -3,31 +3,40 @@ import axios, {
     AxiosError
 } from "../../node_modules/axios/index";
 
-interface Ibook {
-    title : string;
-    artist : string;
-    releaseYear : number;
+interface IRecord {
+    id : number,
+    title : string
+    artist : string
+    releaseYear : number
 }
 
 new Vue({
     el: "#app", 
     data: {
-        books : [],
+        baseUrl: "https://restdrrecordstimm.azurewebsites.net/api/records",
+        records : [],
         errors : [],
-        search : ""
+        responseMsg: null,
+        search : "",
+        editId : null,
+        editMode : false,
+        addPressed : false,
+        addForm: {
+            title : null,
+            artist : null,
+            releaseYear : null
+        },
+        editForm: {
+            title : null,
+            artist : null,
+            releaseYear : null
+        }
     },
 
     created()
     {
         console.log("CREATED")
-        axios.get<Ibook[]>("https://restdrrecordstimm.azurewebsites.net/api/records")
-            .then((response : AxiosResponse<Ibook[]>) => {
-                console.log(response.data)
-                this.books = response.data
-            })
-            .catch((error : AxiosError) => {
-                this.errors = error
-            })
+        this.getAll()
     },
 
     methods: {
@@ -36,16 +45,68 @@ new Vue({
         {
             if(this.search != "" && this.search != null)
             {
-                axios.get<Ibook[]>("https://restdrrecordstimm.azurewebsites.net/api/records?title="+ this.search) 
-                .then((response : AxiosResponse<Ibook[]>) =>{
+                axios.get<IRecord[]>(this.baseUrl+ "?title="+ this.search) 
+                .then((response : AxiosResponse<IRecord[]>) =>{
                     console.log(response.data)
-                    this.books = response.data
+                    this.records = response.data
                 })
                 .catch((error : AxiosError) => {
                     this.errors = error
                 })
             }
-        } 
+        },
 
+        getAll: function()
+        {
+            axios.get<IRecord[]>(this.baseUrl)
+            .then((response : AxiosResponse<IRecord[]>) => {
+                console.log(response.data)
+                this.records = response.data
+            })
+            .catch((error : AxiosError) => {
+                this.errors = error
+            })
+        },
+
+        addRecord: function() 
+        {
+            console.log(this.addForm)
+            axios.post(this.baseUrl, this.addForm)
+            .then((response : AxiosResponse<IRecord>) => {
+                console.log("Added")
+                this.getAll()
+
+            })
+            .catch((error : AxiosError) => {
+                this.errors = error
+            })
+
+        },
+
+        editSaveRecord: function(id : number)
+        {
+            console.log("Rettet " + id)
+            console.log(this.editForm)
+            axios.put<void>(this.baseUrl + "/" + id, this.editForm)
+            .then((response : AxiosResponse) =>{
+                console.log("Rettet " + id)
+                this.editId = null
+            })
+            .catch
+
+        },
+
+        deleteRecord: function(id : number)
+        {
+            if(confirm("Er du sikker på at du vil slette record " + id))
+            {
+                console.log(id)
+                axios.delete<void>(this.baseUrl + "/" + id)
+                .then((response : AxiosResponse) =>{
+                    this.getAll()
+                })
+                .catch
+            }
+        },
     }
 })
